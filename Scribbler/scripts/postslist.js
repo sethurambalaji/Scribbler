04b2f7160d11c2e7bfc95b335fd8a9f342871bf3
sethurambalaji <s.sethurambalaji.s@gmail.com>
2021-05-09 23:47:50 +0530

    Trash feature added

--- Scribbler/scripts/postslist.js
+++ Scribbler/scripts/postslist.js
@@ -2,5 +2,24 @@ var imported = document.createElement('script');
 imported.src = './scommon.js';
 document.head.appendChild(imported);
 
+var posts = document.getElementsByClassName("postCard")
+for(let i=0; i<posts.length; i++){
+    var postId = i+1 ;
+    posts[i].setAttribute("id",'card'+postId);
+}
 
-
+function confirmDeleteModal(value) {
+   var card = document.getElementById(value);
+   var confirmDeleteModal = document.getElementById("confirmDeleteModal").style.display = 'block';
+   document.getElementById("confirmDelete").addEventListener("click", function () {
+       card.parentElement.removeChild(card)
+       closeConfirmDeleteModal();
+   });
+}
+function closeConfirmDeleteModal(){
+    document.getElementById("confirmDeleteModal").style.display = 'none';
+}
+function confirmDelete(card) {
+    card.remove();
+    document.getElementById("confirmDeleteModal").style.display = 'none';
+}

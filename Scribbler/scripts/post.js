0b16020ba6378f32342007daf17ccae697f8b0ee
sethurambalaji <s.sethurambalaji.s@gmail.com>
2021-05-10 16:43:16 +0530

    Like feature Added

--- Scribbler/scripts/post.js
+++ Scribbler/scripts/post.js
@@ -10,7 +10,6 @@ function onEdit() {
   editButton = document.getElementById('edit-button');
   var headingBlock = document.getElementById('edit-heading');
   var postContent = document.getElementById('edit-contentText');
-  //var editIcon = editButton.getElementsByTagName('i')[0];
   if(editButton.innerText==='Edit'){
       headingBlock.setAttribute('contenteditable','true');
       postContent.setAttribute('contenteditable','true');
@@ -24,8 +23,21 @@ function onEdit() {
       postContent.removeAttribute('contenteditable');
       headingBlock.setAttribute('class','edit-heading');
       postContent.setAttribute('class','edit-contentText');
-      //editIcon.setAttribute('class','fa fa-edit');
       editButton.innerHTML = 'Save<i class="fa fa-edit" style="padding-left: 4px;"></i></button>';
   }
+}
+
+var numberOfClicks=0;
+function postLiked(){
+    document.getElementById("likeBlog").innerHTML = "<i class='fa fa-thumbs-up'></i> Liked!";
+    numberOfClicks += 1;
+    if(numberOfClicks!=0){
+        if (numberOfClicks==1){
+            document.getElementById('likeCount').innerHTML = numberOfClicks + " person likes this!";
+        }
+        else {
+            document.getElementById('likeCount').innerHTML = numberOfClicks + " people have liked this!";
+        }
+    }
 }
 

e339bdc31f1dd56ca2366384bcd2f8d4cb7757c9
sethurambalaji <73328800+sethurambalaji@users.noreply.github.com>
2021-05-10 20:16:43 +0530

    Edit button bug fixed

--- Scribbler/scripts/post.js
+++ Scribbler/scripts/post.js
@@ -27,26 +27,21 @@ function onEdit() {
   editButton = document.getElementById('edit-button');
   var headingBlock = document.getElementById('edit-heading');
   var postContent = document.getElementById('edit-contentText');
-      var postContenttext = postContent.innerText;
+  if(editButton.innerText==='Edit'){
       headingBlock.setAttribute('contenteditable','true');
       postContent.setAttribute('contenteditable','true');
       headingBlock.setAttribute('class','edit-heading editable');
       postContent.setAttribute('class','edit-contentText editable');
       editButton.innerHTML = 'Save<i class="fa fa-save" style="padding-left: 4px;"></i></button>';
-      editButton.setAttribute('onclick','onsave()');
-}
-
-function onSave(){
-    var editButton;
-    editButton = document.getElementById('edit-button');
-    var headingBlock = document.getElementById('edit-heading');
-    var postContent = document.getElementById('edit-contentText');
-    headingBlock.removeAttribute('contenteditable');
-    postContent.removeAttribute('contenteditable');
-    headingBlock.setAttribute('class','edit-heading');
-    postContent.setAttribute('class','edit-contentText');
-    editButton.innerHTML = 'Edit<i class="fa fa-edit" style="padding-left: 4px;"></i></button>';
-    editButton.setAttribute('onclick','onEdit()');
+  }
+  else{
+      editButton.innerText = 'Edit' ;
+      headingBlock.removeAttribute('contenteditable');
+      postContent.removeAttribute('contenteditable');
+      headingBlock.setAttribute('class','edit-heading');
+      postContent.setAttribute('class','edit-contentText');
+      editButton.innerHTML = 'Edit<i class="fa fa-edit" style="padding-left: 4px;"></i></button>';
+  }
 }
 
 
